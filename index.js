24613d00bffa2418336eec4213571f6d97305497
Karanveer Singh <82330083+KaranveerManhas@users.noreply.github.com>
2023-09-25 21:09:13 +0530

    Views Added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// Setup views engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 // Use express router
 app.use('/', require('./routes/index'));
 
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,10 @@ const homeController = require('../controller/home_controller');
 console.log('Router Loaded');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
 
-
+// for any further routes, access from here
+// router.use('/routerName', require('./routerFile'));
 
 
 
