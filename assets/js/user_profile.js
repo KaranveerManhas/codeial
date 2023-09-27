71616c4879928fb548324d6fe9232f33f931b8d5
Karanveer Singh <82330083+KaranveerManhas@users.noreply.github.com>
2023-09-27 21:56:45 +0530

    Added basic javascript to user profiles page

---
+++ assets/js/user_profile.js
@@ -0,0 +1,7 @@
+const heading = document.getElementsByTagName('h1')[0];
+console.log(heading);
+
+document.addEventListener('DOMContentLoaded', () => {
+    heading.style.marginTop = "100px";
+    heading.style.marginBottom = "100px";
+});
