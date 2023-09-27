2adf0c0695f05a11c82ed0ff52085607061edfa3
Karanveer Singh <82330083+KaranveerManhas@users.noreply.github.com>
2023-09-27 21:41:46 +0530

    Added access to static files for layouts and pages

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const expressLayout = require('express-ejs-layouts');
 
 app.use(express.static('./assets'));
 app.use(expressLayout);
+// Extract styles and scripts from subpages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 // Setup views engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
